cdc1eab6ecd21480bfd091af004e62ff455317b2
DimidonPlay <45366465+DimidonPlay@users.noreply.github.com>
2018-12-17 09:38:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,68 @@
 const Discord = require('discord.js');
 const cat = new Discord.Client();
-var lotok_zasran = 0; 
-var lotok_timer = new setInterval(function() { lotok_zasran=lotok_zasran+1 }, 36000);
-var lotok_stop = new setTimeout(function() { clearInterval(lotok_timer); msg.channel.send('``` Уважаемые кошки выш лоток засран на 100% ```') }, 3601000);
-var lotok_clear
+var say_string
+var n
+var achat
+const fs = require('fs');
+cat.msgs = require('./chatstory.json');
 
 cat.login('NTE2MzE0MDk4NTk0NjExMjAx.Dtx37A.CcH1UM5QNl6d_PGuimlw3YIqASg');
-cat.on('message', (msg) => { 
-    if (msg.content == '+wash toilet') {
-        msg.delete(msg.lastMessage); 
-        msg.channel.send('``` +wash toilet | Лоток успешно помыт ```'); 
-        lotok_zasran=0;
-        lotok_timer = new setInterval(function() { lotok_zasran=lotok_zasran+1 }, 36000);
-        lotok_stop = new setTimeout(function() { cleatInterval(lotok_timer) }, 3601000);
+cat.on('message', (msg) => {
+    if (msg.guild.id = '508920768273580053') {
+        console.log(msg.guild.id);
+    if (msg.content != '+clear§') {
+    if (cat.msgs[msg.author.username] != undefined) {
+        let achat = cat.msgs[msg.author.username].msg;
+        cat.msgs [msg.author.username] = {
+            msg: (achat+'`,`'+msg.content)
+    } 
+    } else {
+        cat.msgs [msg.author.username] = {
+            msg: ('`'+msg.content) 
     }
-    if (msg.content == '+cheek poop') {
+    }
+    if (msg.author.username != 'CatBot') {
+    fs.writeFile('./chatstory.json', JSON.stringify(cat.msgs, null, 4), err => {
+        if (err) console.log(err); else console.log('message "'+msg.content+'" saved succeful')
+    })}} else {
+        cat.msgs = cat.msgs = {
+                msg: ('`last clared by '+msg.author.username)
+        }
         msg.delete(msg.lastMessage);
-        msg.channel.send('``` +cheek poop | Процент засранности лотка: '+lotok_zasran+'% ```'); 
+        fs.writeFile('./chatstory.json', '{}', err => {
+            if (err) console.log(err); else console.log('messages cleared succeful')
+        })
+
+        
+        /*cat.msgs [msg.author.username] = {
+            msg: ('')
+        }
+        fs.writeFile('./chatstory.json', JSON.stringify(cat.msgs, null, 4), err => {
+            if (err) console.log(err) })*/
     }
-    if (msg.content == '+coin flip') {
+
+    if (msg.content == '+coinflip') {
+        msg.delete(msg.lastMessage);
         if ((Math.round(Math.random() * (0 - 1) + 1)) == 0) {
         msg.channel.send('Выйграл');
         } else {msg.channel.send('Проиграл')}
     }
-    lotok_clear = new setTimeout(function() { msg.channel.messages.deleteAll() }, 10000);
+    if ((msg.content.startsWith('§bot§')) && (msg.content.length > 5)) {
+        achat=msg.content;
+        msg.delete(msg.lastMessage);
+        say_string='';
+        for (var n = 0; n != (achat.length - 5); n++) {
+            say_string=say_string+achat[5+n];
+        };
+        if (msg.member.roles.some(r=>["Кошачий вождь", "Коты модераторы"].includes(r.name))) {
+            msg.channel.send('**'+say_string+'**');
+        } else {
+            if (msg.member.roles.some(r=>["Коты няшки", "Просто коты"].includes(r.name))) {
+                msg.channel.send('**User | '+say_string+'**');
+            } else {
+                msg.reply('You cant using me for chat');
+            }
+        }
+    }
+}
 });
